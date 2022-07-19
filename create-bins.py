9c1ea884ffd082979c6250a81652cc4ab58f19a0
jlazovskis <jlazovskis@gmail.com>
2022-07-19 11:48:43 +0200

    description

--- create-bins.py
+++ create-bins.py
@@ -1,3 +1,8 @@
+# Step 1 of 4: Create k-dimensional bins of neurons, using k selection paramaters and a kd-tree
+
+# Number of bins is dependent on configuration file, which specifies bin size
+# Number of bins is 2^n for the smallest integer n such that 31346/2^n <= (bin size) 
+
 # Load packages
 print('Loading packages...', flush=True)
 import json
