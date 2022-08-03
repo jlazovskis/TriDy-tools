466ac2ea4ad2e6a41303e2a2202df0f4f51931a8
jlazovskis <jlazovskis@gmail.com>
2022-08-03 15:46:28 +0200

    bug fixes

--- create-runfiles.py
+++ create-runfiles.py
@@ -29,7 +29,6 @@ with open(config_address, 'r') as f:
 
 selection_parameters = config_dict['selection_parameters']        # List of selection parameters to use. Should match names of custom selection parameter binary arrays
 feature_parameters = config_dict['feature_parameters']            # List of feature parameters to use. One collection of files will be created for each
-feature_gaps = config_dict['feature_gaps']                        # List of feature parameter gaps to use. Must be of same length as feature_parameters. Irrelevant for all but spectral gaps
 json_template = config_dict['json_template']                      # Template to use when creating configuration .json files
 sbatch_template = config_dict['sbatch_template']                  # Template to use when creating .sbatch files to begin jobs
 num_jobs = config_dict['num_jobs']                                # Number of jobs (=number of sbatch files) to split up featursation+classification task into
@@ -38,6 +37,30 @@ bin_dir = config_dict['bin_dir']                                  # Location of 
 parameter_dir = config_dict['parameter_dir']                      # Location of parameters created in step 2. Default is ./parameters/
 tridy_dir = config_dict['tridy_dir']                              # Location of TriDy package as it is on github. Default is ./../TriDy/
 runfile_dir = config_dict['runfile_dir']                          # Where to export the runfiles. Default is ./runfiles/
+
+# Get feature gaps from names
+feature_gaps = []
+for p in feature_parameters:
+    if (p == "asg") or (p == "asg_high"):
+        feature_gaps.append("high")
+    elif (p == "tpsg") or (p == "tpsg_high"):
+        feature_gaps.append("high")
+    elif (p == "tpsg_reversed") or (p == "tpsg_reversed_high"):
+        feature_gaps.append("high")
+    elif (p == "clsg") or (p == "clsg_low"):
+        feature_gaps.append("low")
+    elif (p == "blsg") or (p == "blsg_high"):
+        feature_gaps.append("high")
+    elif (p == "blsg_reversed") or (p == "blsg_reversed_high"):
+        feature_gaps.append("high")
+    elif "high" in p:
+        feature_gaps.append("high")
+    elif "low" in p:
+        feature_gaps.append("low")
+    elif "radius" in p:
+        feature_gaps.append("radius")
+    else:
+        feature_gaps.append("")
 
 created_file_counter = 0
 created_directory_counter = 0
@@ -50,6 +73,14 @@ print('Loading parameter names', flush=True)
 with open('data/parameters-shortnames.pickle', 'rb') as f:
     df_shortdict = pickle.load(f)
 
+# Dictionary for translating to pipeline names
+fparam_to_pipename = {"fcc":"ccc"}
+for name in ["ec","tribe_size","deg","in_deg","out_deg","rc","rc_chief","tcc","nbc","dc2","dc3","dc4","dc5","dc6","binary"]:
+    fparam_to_pipename[name] = name
+for spectrum in ["asg","tpsg", "tpsg_reversed", "clsg", "blsg", "blsg_reversed"]:
+    for gap in ["", "_high", "_low", "_radius"]:
+        fparam_to_pipename[spectrum+gap] = spectrum
+
 ##
 ## Load function
 ##
@@ -58,15 +89,16 @@ print('Loading helper function', flush=True)
 
 # Function to replace strings in files
 # Modified from https://stackoverflow.com/questions/4128144
-def file_string_replace(source_file, target_file, old_new_strings):
+def file_string_replace(source_file, target_file, old_new_strings, verbose=False):
 
     # Safely read the input filename using 'with'
     with open(source_file) as f:
         s = f.read()
-        for old_string,new_string in old_new_strings:
-            if old_string not in s:
-                print('"{old_string}" not found in {source_file}, exiting.'.format(**locals()))
-                return
+        if verbose:
+            for old_string,new_string in old_new_strings:
+                if old_string not in s:
+                    print('"{old_string}" not found in {source_file}, exiting.'.format(**locals()))
+                    return
 
     # Safely write the changed content, if found in the file
     with open(target_file, 'w') as f:
@@ -86,27 +118,29 @@ for sparam in selection_parameters:
     try:
         current_bins = np.load(expected_bins,allow_pickle=True)
         num_bins = len(current_bins)
-        print('Selection parameter '+sparam+' has '+len(num_bins)+' bins', flush=True)
+        print('Selection parameter '+sparam+' has '+str(num_bins)+' bins', flush=True)
     except:
-        print('Expected bin file '+expected_bins+' not found. Check bin_dir in conig file. Exiting.', flush=True)
+        print('Expected bin file '+expected_bins+' not found. Check bin_dir in config file. Exiting.', flush=True)
         exit()
 
     # Iterate over feature parameters
     for findex,fparam in enumerate(feature_parameters):
-        fshort = df_shortdict[fparam]
+        try:
+            fshort = df_shortdict[fparam]
+        except:
+            fshort = df_shortdict[reduce(lambda x,y: x+'_'+y,fparam.split('_')[:-1])]
         fgap = feature_gaps[findex]
-        assert fgap in ['high','low','radius'], 'Feature gap must be one of \'high\', \'low\', \'radius\'.' 
+        assert fgap in ['', 'high','low','radius'], 'Feature gap must be one of \'\', \'high\', \'low\', \'radius\'.' 
         current_name = sparam+'-'+fshort
         print(current_name, end='', flush=True)
 
         # Create folders
-        os.mkdir('configs/'+current_name)
-        os.mkdir('sbatches/'+current_name)
-        os.mkdir('results/'+current_name)
-        created_directory_counter += 3
-
-        # Declare string replacements
-        string_replacements = [('#SSHORT',sparam), ('#FPARAM',fparam), ('#FSHORT',fshort), ('#FGAP',fgap)]
+        for parent_dir in ['configs','sbatches','results']:
+            try:
+                os.mkdir(parent_dir+'/'+current_name)
+                created_directory_counter += 1
+            except:
+                pass
 
         # Distribute jobs evenly
         chunk_size = num_bins//num_jobs
@@ -121,6 +155,9 @@ for sparam in selection_parameters:
         f = open(runfile_dir+current_name+'.sh','w')
 
         for jobnum in range(num_jobs):
+            # Declare string replacements
+            string_replacements = [('#SSHORT',sparam), ('#FPARAM',fparam_to_pipename[fparam]), ('#FSHORT',fshort), ('#FGAP',fgap)]
+
             print(' '+str(jobnum), end='', flush=True)
             string_replacements.append(('#JOBNUM',str(jobnum)))
             string_replacements.append(('#SPARAMS',reduce(lambda x,y: x+'\", \"'+y, [sparam+'-'+str(i) for i in range(chunks_sum[jobnum],chunks_sum[jobnum+1])])))
@@ -147,17 +184,21 @@ for sparam in selection_parameters:
     toolbox_replacements = [(
         # Custom selection parameter names in dictionary
         'param_dict_inverse = {',
-        'for i in range('+str(num_bins)+'):\n    param_dict[\''+sparam+'-'+str(i)+'\']=\''+sparam+'-'+str(i)+'\'\n\nparam_dict_inverse = {'
+        'for i in range('+str(num_bins)+'):\n    param_dict[\''+sparam+'-\'+str(i)]=\''+sparam+'-\'+str(i)\n\nparam_dict_inverse = {'
         ),(
         # Load custom selection parameters
         'param_files = [np.load(dir_export+\'individual_parameters/\'+param_dict_inverse[f]+\'.npy\',allow_pickle=True) for f in param_names]\n',
-        'param_files = []\nfor f in param_names:\n    try:\n        param_files.append(np.load(dir_export+\'individual_parameters/\'+param_dict_inverse[f]+\'.npy\',allow_pickle=True))\n    except:\n        param_files.append(np.load(./../Tridy-tools'+parameter_dir[1:]+'\'+param_dict_inverse[f]+\'.npy\',allow_pickle=True))\n'
+        'param_files = []\nfor f in param_names:\n    try:\n        param_files.append(np.load(dir_export+\'individual_parameters/\'+param_dict_inverse[f]+\'.npy\',allow_pickle=True))\n    except:\n        param_files.append(np.load(\'./../TriDy-tools'+parameter_dir[1:]+'\'+param_dict_inverse[f]+\'.npy\',allow_pickle=True))\n'
         )]
     file_string_replace(tridy_dir+'toolbox.py', runfile_dir+'toolbox-'+sparam+'.py', toolbox_replacements)
     created_file_counter += 1
 
     # Copy and modify pipeline.py file
     pipeline_replacements = [(
+        # Open modified toolbox file
+        'exec(open(\'toolbox.py\').read())',
+        'exec(open(\'../TriDy-tools'+runfile_dir[1:]+'toolbox-'+sparam+'.py\').read())'
+        ),(
         # Declare job number
         '\'bin_number\']\n',
         '\'bin_number\']\njob_order = config_dict[\'values\'][\'job_order\']\n'
